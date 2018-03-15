0718414fec792a350c0bdd735a88b683618cb1ce
ZhaoYuqiao <zhao.migang.com>
2018-03-15 09:53:00 +0800

    Initial Commit

---
+++ MGRequestBuilder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MGRequestBuilder
+//
+//  Created by zhao on 2018/3/15.
+//  Copyright © 2018年 zhao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MGRequestBuilder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MGRequestBuilder
+//
+//  Created by zhao on 2018/3/15.
+//  Copyright © 2018年 zhao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
